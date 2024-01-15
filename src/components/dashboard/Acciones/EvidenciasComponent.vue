34e91d85bb13c46047ff4b6fe7471fcf6358a561
sagonzalez76 <sagonzalez76@misena.edu.co>
2024-01-15 02:11:26 -0500

    add evidence progress

--- src/components/dashboard/Acciones/EvidenciasComponent.vue
+++ src/components/dashboard/Acciones/EvidenciasComponent.vue
@@ -10,12 +10,12 @@ const token = useAuthStore().token
 const action = useActionStore().action;
 const archivosInput = ref(null);
 const evidences = ref({})
-
+const servidor = "http://localhost:8000/uploads/"
 
 
 onMounted(() => {
     getEvidences(action.id)
-console.log(action.id)
+    console.log(action.id)
     // new DataTable('#example');
 })
 
@@ -24,7 +24,7 @@ console.log(action.id)
 const getEvidences = async () => {
 
     await axios.get(`http://localhost:8000/actions/${action.id}/evidences`, {
-        
+
         headers: {
             'Authorization': `Bearer ${token}`,
         },
@@ -36,6 +36,9 @@ const getEvidences = async () => {
         .catch((error) => {
             console.log(error)
         })
+
+
+
 }
 
 
@@ -52,6 +55,8 @@ const subirArchivos = async () => {
     console.log(formData);
 
     try {
+
+        mostrarModal()
         await axios.post('http://localhost:8000/evidences', formData, {
             headers: {
                 'Content-Type': 'multipart/form-data',
@@ -66,21 +71,46 @@ const subirArchivos = async () => {
         // Lógica adicional después de subir los archivos
     } catch (error) {
         console.error('Error al subir archivos:', error.response.data);
+    } finally {
+        cerrarModal()
     }
 };
 
 
+const isDocument = (type) => {
+    return (
+        type.startsWith('application/pdf') ||
+        type.startsWith('application/msword') ||
+        type.startsWith('application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+        // Agrega otros tipos de documentos según sea necesario
+        // ...
+    );
+};
 
 
+const modalVisible = ref(false);
+
+// Función para mostrar el modal
+const mostrarModal = () => {
+    modalVisible.value = true;
+};
+
+// Función para cerrar el modal
+const cerrarModal = () => {
+    // Aquí puedes agregar lógica adicional antes de cerrar el modal, si es necesario.
+    modalVisible.value = false;
+};
 
 
 </script>
 
 
 <template>
-    <body>
+    <button @click="mostrarModal">Abrir</button>{{ modalVisible }}
 
 
+
+    <body class="container" style="max-width: 100%;">
         <header class="mb-4">
             <section>
 
@@ -94,41 +124,71 @@ const subirArchivos = async () => {
                 <h4>{{ action.updatedAt }}</h4>
             </section>
             <section>
-
                 <button type="button" class="btn btn-outline-primary ms-auto" data-bs-toggle="modal"
                     data-bs-target="#crearAccionModal">Crear Evidencia</button>
             </section>
-
         </header>
 
         <main class="my-5">
             <h1>Imagenes</h1>
-            <ul>
-                <li>
-                    uploads\eb3ced79a4b576eba1fad23da78e153b
-                    <img src="http://localhost:8000/uploads/eb3ced79a4b576eba1fad23da78e153bg" alt="Mi Archivo">
-                   {{evidences}}
-                </li>
-            </ul>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div v-for="evidence in evidences" :key="evidence.id">
+                    <div v-if="evidence.type.startsWith('image/')">
+                        <div class="card" style="height: 18rem; overflow: hidden;">
+                            <img :src="servidor + evidence.url" class="card-img-top img-fluid" alt="..."
+                                style="width: 100%; height: 100%; object-fit: cover;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h1>Audios</h1>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div v-for="evidence in evidences" :key="evidence.id">
+                    <div v-if="evidence.type.startsWith('audio/')">
+                        <div class="card" style="width: 18rem;">
+                            <audio controls :src="servidor + evidence.url"></audio>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <h1>Videos</h1>
-            <ul>
-                <li>
-                    Li
-                </li>
-            </ul>
+            <div class="row row-cols-1 row-cols-md-4 g-4">
+                <div v-for="evidence in evidences" :key="evidence.id">
+                    <div v-if="evidence.type.startsWith('video/')">
+                        <div class="card" style="width: 18rem;">
+                            <video controls :src="servidor + evidence.url"></video>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ evidence.name }}</h5>
+                                <p class="card-text">{{ evidence.createdAt }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             <h1>Documentos</h1>
-            <ul>
-                <li>
-                    Li
-                </li>
-            </ul>
-
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div v-for="evidence in evidences" :key="evidence.id">
+                    <div v-if="isDocument(evidence.type)">
+                        <div class="card" style="width: 18rem;">
+                            <a :href="servidor + evidence.url" target="_blank" rel="noopener noreferrer" download>
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ evidence.type }}</h5>
+                                    <p class="card-text">{{ evidence.createdAt }}</p>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
+
+
         <footer>
-
+            <!-- Contenido del pie de página -->
         </footer>
-
 
         <div class="modal fade" id="crearAccionModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
             aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -139,25 +199,20 @@ const subirArchivos = async () => {
                         <button type="button" id="cerrarBotonCrear" class="btn-close" data-bs-dismiss="modal"
                             aria-label="Close"></button>
                     </div>
-
-
                     <form class="modal-body" @submit.prevent="subirArchivos()">
-
                         <input type="file" ref="archivosInput" class="form-control" id="floatingInputGrid" placeholder=""
                             multiple required>
-
                         <div class="modal-footer">
                             <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Volver</button>
                             <button type="submit" class="btn btn-success">Crear Accion</button>
-
                         </div>
                     </form>
-
-
-
                 </div>
             </div>
         </div>
+
+
+
 
     </body>
 </template>
